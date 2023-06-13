cfed68232b57a3a8ab06ee75ff6a8cfcb9236573
DimaSimakov <dimarodeo@gmail.com>
2023-06-13 19:16:22 +0700

    added new function

--- src/hh_service.py
+++ src/hh_service.py
@@ -1,4 +1,8 @@
 from abc import ABC
+import requests
+import json
+import os
+
 
 from src.main import Vacancies
 
@@ -7,5 +11,29 @@ class HeadHunterApi(Vacancies, ABC):
     """
     Класс реализует получение вакансий с портала Head Hunter
     """
+    def __init__(self, keyword):
+        self.keyword = keyword  # ключевое слово, по которому ведется поиск вакансии
+        self.url = 'https://api.hh.ru/vacancies'  # адрес запроса вакансий
+
     def get_vacancies(self):
-        pass
+        """
+        Получаем список вакансий по запросу
+        :return:
+        """
+
+        # формируем справочник для параметров GET-запроса
+        params = {
+            'text': self.keyword,  # Текст фильтра (ключевое слово)
+            'area': 1,  # Поиск ощуществляется по вакансиям города Москва
+            'page': 0,  # Индекс страницы поиска на HH
+            'per_page': 10  # Кол-во вакансий на 1 странице
+        }
+
+        req = requests.get(self.url, params)  # Посылаем запрос к API
+        data_in = req.content.decode()  # Декодируем его ответ, чтобы Кириллица отображалась корректно
+        req.close()
+
+        data_out = json.loads(data_in)  # преобразуем полученные данные из формата json
+
+        return data_out
+
