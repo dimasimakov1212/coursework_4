1bd9b05e68cb99bef67e75d0a81765c478de11ec
DimaSimakov <dimarodeo@gmail.com>
2023-06-14 20:12:44 +0700

    optimized function

--- src/hh_service.py
+++ src/hh_service.py
@@ -16,13 +16,6 @@ class HeadHunterApi(Vacancies, ABC):
         self.keyword = keyword  # ключевое слово, по которому ведется поиск вакансии
         self.url_api = 'https://api.hh.ru/vacancies'  # адрес запроса вакансий через API
         self.vacancies_list = []  # список, в который будут сохраняться вакансии по запросу
-        # self.vacancy_id = None  # id вакансии
-        # self.vacancy_name = None  # название вакансии
-        # self.vacancy_salary_from = None  # нижний предел зарплаты
-        # self.vacancy_salary_to = None  # верхний предел зарплаты
-        # self.vacancy_url = None  # ссылка на вакансию
-        # self.vacancy_description = None  # описание вакансии
-        # self.vacancy_experience = None  # опыт работы
 
     def get_vacancies(self):
         """
@@ -53,35 +46,27 @@ class HeadHunterApi(Vacancies, ABC):
             # req = requests.get(self.url, params)  # Посылаем запрос к API без headers
 
             req = requests.get(self.url_api, params, headers=headers)  # Посылаем запрос к API
-            data_in = req.content.decode()  # Декодируем ответ API, чтобы Кириллица отображалась корректно
-            req.close()
 
-            data_out = json.loads(data_in)  # преобразуем полученные данные из формата json
+            if req.status_code == 200:  # проверяем на корректность ответа
 
-            # vacancy_dict = {}  # словарь для данных о вакансии
+                data_in = req.content.decode()  # Декодируем ответ API, чтобы Кириллица отображалась корректно
+                req.close()
 
-            # полученные вакансии складываем в словарь и добавляем его в список
-            for vacancy in data_out['items']:
+                data_out = json.loads(data_in)  # преобразуем полученные данные из формата json
 
-                # запускаем метод формирования словаря
-                vacancy_dict = HeadHunterApi.get_vacancy_dict(self, vacancy)
+                # полученные вакансии складываем в словарь и добавляем его в список
+                for vacancy in data_out['items']:
 
-                # vacancy_dict['id'] = vacancy['id']  # id вакансии
-                # vacancy_dict['name'] = vacancy['name']  # название вакансии
-                # if vacancy['salary']:
-                #     vacancy_dict['salary_from'] = vacancy['salary']['from']  # нижний предел зарплаты
-                #     vacancy_dict['salary_to'] = vacancy['salary']['to']  # верхний предел зарплаты
-                # else:
-                #     vacancy_dict['salary_from'] = None
-                #     vacancy_dict['salary_to'] = None
-                # vacancy_dict['vacancy_url'] = vacancy['alternate_url']  # ссылка на вакансию
-                # vacancy_dict['description'] = vacancy['snippet']['responsibility']  # описание вакансии
-                # vacancy_dict['experience'] = vacancy['experience']['name']  # требуемый опыт работы
+                    # запускаем метод формирования словаря
+                    vacancy_dict = HeadHunterApi.get_vacancy_dict(self, vacancy)
 
-                # print(vacancy)
-                self.vacancies_list.append(vacancy_dict)  # полученный словарь добавляем в список
+                    # print(vacancy)
+                    self.vacancies_list.append(vacancy_dict)  # полученный словарь добавляем в список
 
-            time.sleep(0.25)  # временная задержка во избежание блокировки большого количества запросов
+            else:
+                print("В настоящий момент сайт недоступен. Попробуйте позже.")
+
+            time.sleep(0.2)  # временная задержка во избежание блокировки большого количества запросов
 
         return self.vacancies_list
 
@@ -112,12 +97,3 @@ test_print = test_1.get_vacancies()
 # print(test_1.vacancies_list)
 for item in test_1.vacancies_list:
     print(item)
-
-# print(test_print['items'][0])  # весь словарь
-# print(test_print['items'][0]['id'])  # id вакансии
-# print(test_print['items'][0]['name'])  # название вакансии
-# print(test_print['items'][0]['salary']['from'])  # зарплата от
-# print(test_print['items'][0]['salary']['to'])  # зарплата до
-# print(test_print['items'][0]['alternate_url'])  # ссылка на вакансию
-# print(test_print['items'][0]['snippet']['responsibility'])  # описание вакансии
-# print(test_print['items'][0]['experience']['name'])  # опыт работы
