2a8563a50adcfe920de1ce487c2dc42fb4a8ed7b
DimaSimakov <dimarodeo@gmail.com>
2023-06-15 18:48:17 +0700

    add new class for new site

--- src/sj_service.py
+++ src/sj_service.py
@@ -1,3 +1,22 @@
 # Параметры доступа
 # ID	2604
 # Secret key	v3.r.137614806.f1965cb543def5c60782496be7a7ebc821b88608.5aaca3901c30bd49f51e0eebff148e7a02ac878d
+
+from abc import ABC
+import requests
+import json
+import os
+import time
+
+
+from src.main import Vacancies
+
+
+class SuperJobAPI(Vacancies, ABC):
+    """
+    Класс реализует получение вакансий с портала Superjob
+    """
+    def __init__(self, keyword):
+        self.keyword = keyword  # ключевое слово, по которому ведется поиск вакансии
+        self.url_api = 'https://api.superjob.ru/2.0/vacancies'  # адрес запроса вакансий через API
+        self.vacancies_list = []  # список, в который будут сохраняться вакансии по запросу
