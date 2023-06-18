64f1120b1fc7f69fabefc05e86a85e5afd0e4151
DimaSimakov <dimarodeo@gmail.com>
2023-06-18 20:31:48 +0700

    corrected main function

--- src/main.py
+++ src/main.py
@@ -20,13 +20,41 @@ def general_function():
 
     if choice_portal == 1:
         # инициализируем поиск вакансий на Head Hunter по поисковому запросу
-        get_hh_list = HeadHunterApi(search_text)
-
-        get_hh_list.get_vacancies()  # получаем список вакансий с сайта в формате json
-
-        hh_list = get_hh_list.vacancies_list  # получаем преобразованный список
-
+        hh_list = get_hh_vacancies(search_text)
         return print(hh_list)
+
+    if choice_portal == 2:
+        # инициализируем поиск вакансий на Super Job по поисковому запросу
+        sj_list = get_sj_vacancies(search_text)
+        return print(sj_list)
+
+
+def get_hh_vacancies(search_text):
+    """
+    Получает список вакансий с Head Hunter
+    :return:
+    """
+    get_hh_list = HeadHunterApi(search_text)
+
+    get_hh_list.get_vacancies()  # получаем список вакансий с сайта в формате json
+
+    hh_list_out = get_hh_list.vacancies_list  # получаем преобразованный список
+
+    return hh_list_out
+
+
+def get_sj_vacancies(search_text):
+    """
+    Получает список вакансий с SuperJob
+    :return:
+    """
+    get_sj_list = SuperJobAPI(search_text)
+
+    get_sj_list.get_vacancies()  # получаем список вакансий с сайта в формате json
+
+    sj_list_out = get_sj_list.vacancies_list  # получаем преобразованный список
+
+    return sj_list_out
 
 
 if __name__ == '__main__':
