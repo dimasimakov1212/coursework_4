5dea87d177cb04c940336a5f1f51d4cf23298a27
DimaSimakov <dimarodeo@gmail.com>
2023-06-20 21:34:16 +0700

    add class init

--- src/vacancies_control.py
+++ src/vacancies_control.py
@@ -19,10 +19,21 @@ class VacanciesControl:
     def __init__(self, vacancies_all):
         self.vacancies_all = vacancies_all
 
+        self.vacancy_id = self.vacancies_all['id']  # id вакансии
+        self.vacancy_name = self.vacancies_all['name']  # название вакансии
+        self.vacancy_salary_from = self.vacancies_all['salary_from']  # нижний предел зарплаты
+        self.vacancy_salary_to = self.vacancies_all['salary_to']  # верхний предел зарплаты
+        self.vacancy_currency = self.vacancies_all['currency']  # валюта зарплаты
+        self.vacancy_employer = self.vacancies_all['employer']  # наименование работодателя
+        self.vacancy_vacancy_url = self.vacancies_all['vacancy_url']  # ссылка на вакансию
+        self.vacancy_description = self.vacancies_all['description']  # описание вакансии
+        self.vacancy_experience = self.vacancies_all['experience']  # требуемый опыт работы
+
     def __repr__(self):
         return f"{self.__class__.__name__}" \
                f"{self.vacancies_all}"
 
     def __len__(self):
         return len(self.vacancies_all)
+
 
